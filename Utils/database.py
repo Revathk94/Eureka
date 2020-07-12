9120f465a2748a097f8ed2d52ca36d6893e843a1
Revathi Kannan <66987838+Revathk94@users.noreply.github.com>
2020-07-12 11:39:34 +0530

    Add files via upload

---
+++ Utils/database.py
@@ -0,0 +1,21 @@
+"""
+Concerned with storing and retrieving books from a list
+"""
+
+books=[]
+
+def add_book(name, author):
+    books.append({'name':name, 'author': author, 'read': False})
+
+def get_all_books():
+     return books
+
+def mark_the_book_as_read(book_name):
+    for items in books:
+        if items["name"].title() == book_name.title():
+            items["read"] = True
+
+def delete_book(book_name):
+    for items in books:
+        if items["name"].title() == book_name.title():
+            books.remove(items)
